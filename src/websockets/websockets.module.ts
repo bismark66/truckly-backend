dbcbf0ffb172d8ae33663f18fb85ac4ce464038d
Bismark <agyarebismark6@gmail.com>
2026-01-12 14:12:34 +0000

    Add AudioCallGateway to WebSocketsModule providers and exports

--- src/websockets/websockets.module.ts
+++ src/websockets/websockets.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { LocationGateway } from './location.gateway';
 import { ChatGateway } from './chat.gateway';
 import { GatewayFactory } from './gateway.factory';
+import { AudioCallGateway } from './audio';
 
 @Module({
-  providers: [LocationGateway, ChatGateway, GatewayFactory],
-  exports: [LocationGateway, ChatGateway, GatewayFactory],
+  providers: [LocationGateway, ChatGateway, GatewayFactory, AudioCallGateway],
+  exports: [LocationGateway, ChatGateway, GatewayFactory, AudioCallGateway],
 })
 export class WebSocketsModule {}
+
+
