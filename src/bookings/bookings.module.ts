73654390a464de5725d0eff2dbadb1b756bc76de
Bismark <agyarebismark6@gmail.com>
2026-01-17 22:51:09 +0000

    Add ChatModule to BookingsModule imports

--- src/bookings/bookings.module.ts
+++ src/bookings/bookings.module.ts
@@ -3,12 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BookingsService } from './bookings.service';
 import { BookingsController } from './bookings.controller';
 import { Booking } from './entities/booking.entity';
-import { LocationGateway } from '../websockets/location.gateway';
+import { ChatModule } from '../chat/chat.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Booking])],
+  imports: [TypeOrmModule.forFeature([Booking]), ChatModule],
   controllers: [BookingsController],
   providers: [BookingsService],
   exports: [BookingsService],
 })
 export class BookingsModule {}
+
