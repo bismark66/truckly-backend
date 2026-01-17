147871f908f248c12c5206d0f5ff4ba795b84dcd
Bismark <agyarebismark6@gmail.com>
2026-01-17 22:53:33 +0000

    Add DriversModule and ChatModule imports to WebSocketsModule

--- src/websockets/websockets.module.ts
+++ src/websockets/websockets.module.ts
@@ -3,11 +3,16 @@ import { LocationGateway } from './location.gateway';
 import { ChatGateway } from './chat.gateway';
 import { GatewayFactory } from './gateway.factory';
 import { AudioCallGateway } from './audio';
+import { DriversModule } from '../drivers/drivers.module';
+import { ChatModule } from '../chat/chat.module';
 
 @Module({
+  imports: [DriversModule, ChatModule],
   providers: [LocationGateway, ChatGateway, GatewayFactory, AudioCallGateway],
   exports: [LocationGateway, ChatGateway, GatewayFactory, AudioCallGateway],
 })
 export class WebSocketsModule {}
+
+
 
 
