e0c192cb9e893b4d7e4b1c45462eabcacc7527d2
Bismark <agyarebismark6@gmail.com>
2026-02-17 00:56:30 +0000

    Add TransportModule and VehiclesModule to BookingsModule imports

--- src/bookings/bookings.module.ts
+++ src/bookings/bookings.module.ts
@@ -4,9 +4,16 @@ import { BookingsService } from './bookings.service';
 import { BookingsController } from './bookings.controller';
 import { Booking } from './entities/booking.entity';
 import { ChatModule } from '../chat/chat.module';
+import { TransportModule } from '../transport/transport.module';
+import { VehiclesModule } from '../vehicles/vehicles.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Booking]), ChatModule],
+  imports: [
+    TypeOrmModule.forFeature([Booking]),
+    ChatModule,
+    TransportModule,
+    VehiclesModule,
+  ],
   controllers: [BookingsController],
   providers: [BookingsService],
   exports: [BookingsService],
