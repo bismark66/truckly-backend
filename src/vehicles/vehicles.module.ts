99afa72d03ac59631eddf95644bf245483de47d2
Bismark <agyarebismark6@gmail.com>
2026-02-17 01:00:03 +0000

    Add TransportModule to VehiclesModule imports

--- src/vehicles/vehicles.module.ts
+++ src/vehicles/vehicles.module.ts
@@ -4,11 +4,13 @@ import { VehiclesService } from './vehicles.service';
 import { VehiclesController } from './vehicles.controller';
 import { Vehicle } from './entities/vehicle.entity';
 import { FleetOwnersModule } from '../fleet-owners/fleet-owners.module';
+import { TransportModule } from '../transport/transport.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Vehicle]),
     FleetOwnersModule,
+    TransportModule,
   ],
   controllers: [VehiclesController],
   providers: [VehiclesService],
