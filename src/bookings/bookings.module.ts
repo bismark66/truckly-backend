9740accc4cac05aa5a9fb8b5d4f429465374a16b
Bismark <agyarebismark6@gmail.com>
2026-02-21 15:06:38 +0000

    Enhance BookingsModule by adding Driver entity and integrating BookingAllocationService and BookingStateService

--- src/bookings/bookings.module.ts
+++ src/bookings/bookings.module.ts
@@ -1,22 +1,34 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BookingsService } from './bookings.service';
 import { BookingsController } from './bookings.controller';
+import { BookingAllocationService } from './booking-allocation.service';
+import { BookingStateService } from './booking-state.service';
 import { Booking } from './entities/booking.entity';
+import { Driver } from '../drivers/entities/driver.entity';
 import { ChatModule } from '../chat/chat.module';
 import { TransportModule } from '../transport/transport.module';
 import { VehiclesModule } from '../vehicles/vehicles.module';
+import { DriversModule } from '../drivers/drivers.module';
+import { WebSocketsModule } from '../websockets/websockets.module';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { RedisModule } from '../redis/redis.module';
+import { QueuesModule } from '../queues/queues.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Booking]),
+    TypeOrmModule.forFeature([Booking, Driver]),
+    forwardRef(() => QueuesModule),
     ChatModule,
     TransportModule,
     VehiclesModule,
+    DriversModule,
+    forwardRef(() => WebSocketsModule),
+    NotificationsModule,
+    RedisModule,
   ],
   controllers: [BookingsController],
-  providers: [BookingsService],
-  exports: [BookingsService],
+  providers: [BookingsService, BookingAllocationService, BookingStateService],
+  exports: [BookingsService, BookingAllocationService, BookingStateService],
 })
 export class BookingsModule {}
-
