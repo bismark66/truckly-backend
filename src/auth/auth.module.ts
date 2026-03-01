4c2e998151babcce3229cb512318cdbf52a791d4
Bismark <agyarebismark6@gmail.com>
2026-03-01 15:29:55 +0000

    Add user session management and device info middleware to AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
@@ -6,9 +7,12 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './jwt.strategy';
+import { UserSession } from './entities/user-session.entity';
+import { DeviceInfoMiddleware } from '../common/middlewares/deviceInfo.middleware';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([UserSession]),
     UsersModule,
     PassportModule,
     JwtModule.registerAsync({
@@ -24,4 +28,10 @@ import { JwtStrategy } from './jwt.strategy';
   providers: [AuthService, JwtStrategy],
   exports: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(DeviceInfoMiddleware)
+      .forRoutes({ path: 'auth/login', method: RequestMethod.POST });
+  }
+}
